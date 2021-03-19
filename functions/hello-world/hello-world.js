989be42c75dc49285dfda33664ceed0b186bda39
Jordi Dosne <jordi@dosne.net>
2021-03-19 19:35:31 +0100

    fixup! feat: hello world

--- functions/hello-world/hello-world.js
+++ functions/hello-world/hello-world.js
@@ -1,17 +1,22 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+    try {
+        const subject = event.queryStringParameters.name || 'World';
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                    path: event.path,
+                    httpMethod: event.httpMethod,
+                    headers: event.headers,
+                    queryStringParameters: event.queryStringParameters,
+                    body: event.body,
+                    isBase64Encoded: event.isBase64Encoded,
+                }
+            )
+        };
+    } catch (error) {
+        return {statusCode: 500, body: error.toString()};
     }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+};
 
-module.exports = { handler }
+module.exports = {handler};
